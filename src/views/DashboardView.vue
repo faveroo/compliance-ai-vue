90fe925387b7546065d623e79e31385cdd4897e1
faveroo <gabriel290974@gmail.com>
2026-06-12 10:26:32 -0300

    style: ajusta o icone de reprocessamento para ficar mais centralizado

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -243,7 +243,7 @@ const riskLabels = {
                 :disabled="processingDocs[doc.id]"
                 @click="handleReprocess(doc)"
               >
-                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 24 24" fill="none"
+                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 5 24 24" fill="none"
                   stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="me-1">
                   <polyline points="1 4 1 10 7 10"/>
                   <path d="M3.51 15a9 9 0 1 0 .49-3.51"/>
